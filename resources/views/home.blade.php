8d9dfc43a8d2f838bd44e1c4800714a4ff12431d
Daron Adkins <daron.adkins@gmail.com>
2017-07-14 09:29:10 -0500

    login page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,9 +18,13 @@
                             <div class="card" draggable="true">
                                 <header class="card-header is-flex">
                                     <p class="card-header-title">
-                                        {{ $task->client->name }}
+                                        <a href="{{ route('client.show', $task->client->id) }}">{{ $task->client->name }}</a>
                                     </p>
-                                    <p><span class="is-pulled-right"><small>Due: {{ Carbon\Carbon::parse($task->due_date)->format('F d, Y') }}</small></span>
+                                    <p>
+                                        <span class="is-pulled-right">
+                                            <small>Due: {{ Carbon\Carbon::parse($task->due_date)->format('F d, Y') }}
+                                            </small>
+                                        </span>
                                     </p>
                                     <a class="card-header-icon">
       <span class="icon">
@@ -30,17 +34,14 @@
                                 </header>
                                 <div class="card-content">
                                     <div class="content">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis
-                                        mauris.
-                                        <a>@bulmaio</a>. <a>#css</a> <a>#responsive</a>
-                                        <br>
-                                        <small>11:09 PM - 1 Jan 2016</small>
+                                        <p>{{ $task->description }}</p>
+                                        <p><span class="is-pulled-right"><small>Created by: {{ App\User::find($task->created_by)->name }}</small></span></p>
                                     </div>
                                 </div>
                                 <footer class="card-footer">
-                                    <a class="card-footer-item">Save</a>
-                                    <a class="card-footer-item">Edit</a>
-                                    <a class="card-footer-item">Delete</a>
+                                    <a href="{{ route('task.show', $task->id) }}" class="card-footer-item">View</a>
+                                    <a class="card-footer-item">Pass On</a>
+                                    <a class="card-footer-item">Complete</a>
                                 </footer>
                             </div>
                         </div>
